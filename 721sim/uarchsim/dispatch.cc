60fe642a4b4be936ea343296a41cb25fdd2d58f7
Ashwin - <asarath@grendel32.ece.ncsu.edu>
2024-04-15 14:59:09 -0400

    Perfect Value Prediction Enabled

--- 721sim/uarchsim/dispatch.cc
+++ 721sim/uarchsim/dispatch.cc
@@ -205,6 +205,17 @@ void pipeline_t::dispatch() {
       // FIX_ME #8 END
       //********************************************
 
+      // If the instruction is value predicted, prediction must be fed in this stage
+      // As long as the mode is perfect value prediction, the instruction is good, it is not a branch and it has a valid destination, 
+      // it is predicted.
+      // For perfect value prediction, we grab this value from the functional simulator and feed it to the destination physical register.
+      if (PERFECT_VALUE_PREDICTION && PAY.buf[index].good_instruction && !branch_flag && PAY.buf[index].C_valid) {
+         PAY.buf[index].predict_flag = true;
+         actual = get_pipe()->peek(PAY.buf[index].db_index);
+         PAY.buf[index].predicted_value = actual->a_rdst[0].value;
+         REN->write(PAY.buf[index].C_phys_reg, PAY.buf[index].predicted_value);
+      }
+
       // FIX_ME #9
       // Clear the ready bit of the instruction's destination register.
       // This is needed to synchronize future consumers.
@@ -224,9 +235,15 @@ void pipeline_t::dispatch() {
 
       //Destination register: C
       //Check if valid
-      //clear_ready function from the renamer class clears the ready bit of specfied physical register 
+      // Sets ready bit of PRF if the destination register is value predicted.
+      // Clears the ready bit of PRF if the destination is not value predicted.
       if(PAY.buf[index].C_valid){
-         REN->clear_ready(PAY.buf[index].C_phys_reg); 
+         if (PAY.buf[index].predict_flag) {
+            REN->set_ready(PAY.buf[index].C_phys_reg);
+         }
+         else {
+            REN->clear_ready(PAY.buf[index].C_phys_reg); 
+         }
       }
 
       //********************************************
--- 721sim/uarchsim/pipeline.cc
+++ 721sim/uarchsim/pipeline.cc
@@ -373,6 +373,9 @@ pipeline_t::pipeline_t(
   fprintf(stats_log, "IBP_BHR_LENGTH = %d\n", IBP_BHR_LENGTH);
   fprintf(stats_log, "ENABLE_TRACE_CACHE = %d\n", (ENABLE_TRACE_CACHE ? 1 : 0));
 
+  fprintf(stats_log, "\n=== VALUE PREDICTOR ============================================================\n\n");
+  fprintf(stats_log, "VALUE PREDICTOR = %s\n", (PERFECT_VALUE_PREDICTION ? "perfect" : "stride (Project 4 spec. implementation)"));
+
   fprintf(stats_log, "\n=== INTERNAL SIMULATOR STRUCTURES ===============================================\n\n");
 
   fprintf(stats_log, "PAYLOAD_BUFFER_SIZE = %d\n", PAY.get_size());
